b08e4679185d6b7d2efc8cdacc94987de4faa241
Ismail NGUYEN <Ismail.NGUYEN@loccitane.com>
2020-06-03 14:18:43 +0200

    add http constants

---
+++ http_constants.php
@@ -0,0 +1,21 @@
+<?php
+	class Http {
+		const HTTP_OK = 200;
+		const HTTP_CREATED = 201;
+		const HTTP_ACCEPTED = 202;
+		const NOT_MODIFIED = 304;
+		const BAD_REQUEST = 400;
+		const UNAUTHORIZED = 401;
+		const FORBIDDEN = 403;
+		const NOT_FOUND = 404;
+		const NOT_ALOWED = 405;
+		const NOT_ACCEPTABLE = 406;
+		const CONFLICT = 409;
+		const PRECONDITION_FAILED = 412;
+		const EXPECTATION_FAILED = 417;
+		const INTERNAL_SERVER_ERROR = 500;
+		const NOT_IMPLEMENTED = 501;
+		const BAD_GATEWAY = 502;
+		const SERVICE_UNAVAILABLE = 503;
+	}
+?>
